aa5a998cd8b1b74d50acf52552ca2025604db918
Galan <zk.zachkeller@gmail.com>
2026-04-06 21:27:49 -0500

    force fix crash

--- app.config.js
+++ app.config.js
@@ -1,21 +1,16 @@
-export default {
-  expo: {
-    updates: {
-      url: "https://u.expo.dev/54b2831a-4d0a-4d91-a189-a58afe3c3557",
-    },
+export default ({ config }) => ({
+  ...config,
 
-    runtimeVersion: {
-      policy: "appVersion",
-    },
-
-    extra: {
-      eas: {
-        projectId: "54b2831a-4d0a-4d91-a189-a58afe3c3557",
-      },
-
-      // 🔥 THIS IS WHAT FIXES YOUR SUPABASE ISSUE
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    },
+  android: {
+    package: "com.anonymous.geoforgeacademy",
   },
-};
+
+  extra: {
+    eas: {
+      projectId: "54b2831a-4d0a-4d91-a189-a58afe3c3557",
+    },
+
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  },
+});
