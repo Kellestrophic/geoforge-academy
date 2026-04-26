7ae20382d8b812a2d7a61551d280aa04581e6e4d
Galan <zk.zachkeller@gmail.com>
2026-04-26 14:16:40 -0500

    fix lock file sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 Sentry.init({
   dsn: "https://7f328ba8f87c01a14994587f59229820@o4511004691202048.ingest.us.sentry.io/4511287049519104",
+  debug: false,
   tracesSampleRate: 1.0,
 });
 
@@ -12,7 +13,12 @@ function Layout() {
   return (
     <SafeAreaProvider>
       <UserProvider>
-        <Stack />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            animation: "none",
+          }}
+        />
       </UserProvider>
     </SafeAreaProvider>
   );
