80100794590ecd2daf830cf902544c9a3aaf7e47
Galan <zk.zachkeller@gmail.com>
2026-04-27 20:36:45 -0500

    fix lock file sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,17 @@
-import { enableScreens } from "react-native-screens";
-enableScreens(false);
-
 import { UserProvider } from "@/context/UserContext";
-import * as Sentry from "@sentry/react-native";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-Sentry.init({
-  dsn: "https://7f328ba8f87c01a14994587f59229820@o4511004691202048.ingest.us.sentry.io/4511287049519104",
-  debug: false,
-  tracesSampleRate: 1.0,
-});
-
-function Layout() {
+export default function Layout() {
   return (
     <SafeAreaProvider>
       <UserProvider>
         <Stack
           screenOptions={{
             headerShown: false,
-            animation: "none",
-            gestureEnabled: false,
-
-            // 🔥 THESE TWO ARE THE MISSING PIECES
-            presentation: "card",
-            freezeOnBlur: false,
           }}
         />
       </UserProvider>
     </SafeAreaProvider>
   );
 }
-
-export default Sentry.wrap(Layout);
